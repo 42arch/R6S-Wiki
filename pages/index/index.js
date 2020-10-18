fa180cea6026f79640eb1ec578cee854bb3d15fb
rendan <rendan@gisinfo.cn>
2020-10-18 13:16:29 +0800

    使用iview重构

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,32 +1,66 @@
-//index.js
-//获取应用实例
-const app = getApp()
-
+// pages/index/index.js
 Page({
+
+  /**
+   * 页面的初始数据
+   */
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+
   },
-  toOperatorPage: function () {
-    wx.navigateTo({
-      url: '/pages/operator/operator'
-    })
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
   },
-  toOperationPage: function () {
-    wx.navigateTo({
-      url: '/pages/operation/operation'
-    })
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
   },
-  toMapPage: function () {
-    wx.navigateTo({
-      url: '/pages/map/map'
-    })
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
   },
-  toAffiliationPage: function () {
-    wx.navigateTo({
-      url: '/pages/affiliation/affiliation'
-    })
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
   }
 })
